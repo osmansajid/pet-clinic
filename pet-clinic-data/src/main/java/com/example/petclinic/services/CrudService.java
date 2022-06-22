77ebd93bb7b9b9592d84f74e84c043c764bdd955
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2022-06-22 14:25:13 +0600

    Refactor Service Interface to Common Base Interface

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package com.example.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T,ID> {
+    T findById(ID id);
+
+    Set<T> findAll();
+
+    T save(T t);
+
+    void delete(T t);
+
+    void deleteById(ID id);
+}
