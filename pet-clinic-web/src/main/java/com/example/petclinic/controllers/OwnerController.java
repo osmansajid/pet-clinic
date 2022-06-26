dc25348c48f74e4a1aed3f31c1d2374f2feeb296
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2022-06-26 14:59:07 +0600

    Updated owners controller and owner index html

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.services.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private OwnerServiceMap ownerServiceMap;
+
+    public OwnerController(OwnerServiceMap ownerServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+        model.addAttribute("owners",ownerServiceMap.findAll());
         return "owners/index";
     }
 }
