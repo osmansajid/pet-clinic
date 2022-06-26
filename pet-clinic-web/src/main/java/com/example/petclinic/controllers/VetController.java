d8e110c98cef936a1df83b14e24deb5915b16661
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2022-06-26 15:46:11 +0600

    Updated vet controller and vet index html

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.example.petclinic.controllers;
 
+import com.example.petclinic.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    private VetServiceMap vetService;
+
+    public VetController(VetServiceMap vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
